a1da3c2950b04c7d4b6bbf8fb39b1ee6e934e0c9
EvaiGG <2889414582@qq.com>
2023-04-09 08:03:14 +0800

    leetcode_110

--- day5_solution.cpp
+++ day5_solution.cpp
@@ -15,6 +15,15 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
+    Node() : val(0), left(nullptr), right(nullptr){}
+    Node(int x): val(x), left(nullptr), right(nullptr) {}
+    Node(int x, Node *left, Node *right) : val(x), left(left), right(right){}
+};
 class day5_solution{
 
 
@@ -69,6 +78,18 @@ public:
             dummy = dummy->next;
         }
         return res->next;
+    }
+
+    //110 是否是平衡二叉树
+    bool isBalanced(Node *root) {
+        if (root == nullptr) return true;
+        return abs(maxDep(root->left) - maxDep(root->right)) <=1
+        && isBalanced(root->left)
+        && isBalanced(root->right);
+    }
+    int maxDep(Node *root) {
+        if (root == nullptr) return 0;
+        return max(maxDep(root->left) , maxDep(root->right))+1;
     }
 };
 
