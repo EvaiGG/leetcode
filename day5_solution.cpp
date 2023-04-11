36dd6d4f4c3952e7c25d259b4b90d972b2a7a901
EvaiGG <2889414582@qq.com>
2023-04-11 14:47:41 +0800

    leetcode_1041

--- day5_solution.cpp
+++ day5_solution.cpp
@@ -5,6 +5,7 @@
 #include "string"
 #include "iostream"
 #include "vector"
+#include "algorithm"
 
 using namespace std;
 
@@ -97,14 +98,107 @@ public:
         if((p!= nullptr && q == nullptr) || (p == nullptr && q!= nullptr) || p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
+
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t rec = 0;
+        for (int i = 0; i < 32 && n> 0; ++i) {
+            rec |= (n &1) << (31-i);
+            n >>=1;
+        }
+        return rec;
+    }
+
+    Node* dfs(vector<int> &nums, int left, int right) {
+        if (left >= right) return nullptr;
+        int mid = (left + right) /2;
+        Node *root = new Node(nums[mid]);
+        root->left =  dfs(nums, left, mid);
+        root->right = dfs(nums, mid+1, right);
+        return root;
+    }
+
+    Node* sortedArrayToBST(vector<int> &nums) {
+        int size = nums.size();
+        return dfs(nums, 0, size);
+    }
+    int dfsFindMinDepth(Node* root, int &res, int dfsMin) {
+        if (root->left == nullptr && root->right == nullptr) {
+            res = dfsMin < res? dfsMin: res;
+            return res;
+        }
+//        dfsMin ++;
+        if (root->left) {
+            dfsFindMinDepth(root->left, res, ++dfsMin);
+        }
+        --dfsMin;
+        if(root->right) {
+            dfsFindMinDepth(root->right, res, ++dfsMin);
+        }
+        --dfsMin;
+        return res;
+    }
+    int minDepth(Node* root) {
+        int res = 0x3f3f3f3f, dfsMin = 1;
+//        Node* dummy = root;
+        if(!root) return 0;
+        return dfsFindMinDepth(root, res, dfsMin );
+    }
+
+    /**
+     * leetcode 1041
+     * @param instructions 指令数组 G 直走一个单位 L:左转90度 R:右转90度
+     * @return
+     */
+    bool isRobotBounded(string instructions) {
+        string ins = instructions + instructions + instructions + instructions;
+        // 方向 逆时针排列
+        vector<char> direction{'N', 'W', 'S', 'E'};
+        vector<int> cursor{0,0};
+        int currentDirection = 1;
+        for (int i = 0; i < ins.size() ; ++i) {
+            char in = ins.at(i);
+            if (in == 'R') {
+                currentDirection = currentDirection-1>0? currentDirection-1:4;
+            }
+            if (in == 'L') {
+                currentDirection = currentDirection+1<=4?currentDirection+1:1;
+            }
+            if (in == 'G') {
+                switch (currentDirection) {
+                    case 1:
+                        cursor[1]+=1;
+                        break;
+                    case 2:
+                        cursor[0]-=1;
+                        break;
+                    case 3:
+                        cursor[1]-=1;
+                        break;
+                    case 4:
+                        cursor[0]+=1;
+                        break;
+                }
+            }
+        }
+        if (cursor[0] == 0 && cursor[1] == 0) return true;
+        return false;
+    }
 };
 
 
 
 int main() {
-//    day5_solution *solution = new day5_solution;
+    day5_solution *solution = new day5_solution;
 //    string res = solution->baseNeg2(9);
 //    cout << res << endl;
 
+    Node* head = new Node(1);
+    Node *p2 = head->left = new Node(2);
+    Node *p3 = head->right = new Node(3);
+    Node *p4 = p2->left = new Node(4);
+    Node *p5 = p2->right = new Node(5);
 
+    int res = solution->minDepth(head);
+
+    cout << res << endl;
 }
