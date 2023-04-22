c4dab71bb55adc18f67d8e5fb7f5f84c3a5d3f6a
EvaiGG <2889414582@qq.com>
2023-04-22 20:55:21 +0800

    1027

--- day8_solution.cpp
+++ day8_solution.cpp
@@ -31,14 +31,44 @@ public:
             curr = right - left;
             right++;
             left = right;
-            res =max(curr, res);
+            res =std::max(curr, res);
+//            std::stoi();
         }
         return res+1;
+    }
+
+    /**
+     * 1027 最长等差数列
+     * @param nums  nums.length >= 2;
+     * @return
+     */
+    // 1027
+    int longestArithSeqLength(vector<int>& nums) {
+        auto [minit, maxit] = minmax_element(nums.begin(),  nums.end());
+
+        int diff =  *maxit - *minit;
+        int ans = 1;
+        for (int d = -diff; d <= diff; ++d) {
+            vector<int> f(*maxit+1, -1);
+            for (const auto &num : nums){
+                int prev = num -d;
+                if (prev >= *minit && prev <= *maxit && f[prev] != -1) {
+                    f[num] = max(f[num], f[prev] +1);
+                    ans = max(f[num], ans);
+                }
+                f[num] = max(f[num], 1);
+            }
+        }
+        return ans;
+
     }
 };
 
 int main() {
-    day8_solution solu;
-    int n =  solu.longestContinuousSubstring("abcdefg");
-    cout << n << endl;
+//    day8_solution solu;
+//    int n =  solu.longestContinuousSubstring("abcdefg");
+//    std::cout << n << endl;
+
+    int n;
+    cin >> n;
 }
