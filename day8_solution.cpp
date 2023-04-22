87da38ddbfe5c4cb01f90b22cb63e422e9ba5acf
EvaiGG <2889414582@qq.com>
2023-04-22 21:23:12 +0800

    1026_dfs_节点祖先节点最大差值

--- day8_solution.cpp
+++ day8_solution.cpp
@@ -5,8 +5,17 @@
 //#include "string"
 #include "algorithm"
 #include "iostream"
+//#include ""
 using namespace std;
 
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class day8_solution{
 public:
     int smallestEvenMultiple(int n) {
@@ -61,6 +70,25 @@ public:
         }
         return ans;
 
+    }
+
+    /**
+     * dfs 1026
+     * @param root
+     * @return
+     */
+    int maxAncestorDiff(TreeNode *root){
+        return maxDiff(root, root->val, root->val);
+    }
+
+    int maxDiff(TreeNode *root, int mi, int ma) {
+        if (root == nullptr) return 0;
+        int diff = max(abs(root->val - mi), abs(root->val - ma));
+        mi = min(mi, root->val);
+        ma = max(ma, root->val);
+        diff = max(diff, maxDiff(root->left, mi, ma));
+        diff = max(diff, maxDiff(root->right, mi, ma));
+        return diff;
     }
 };
 
