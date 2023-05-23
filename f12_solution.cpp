fc3391a8468d7ec06c2a819f96b4f9e00c04d9f5
EvaiGG <2889414582@qq.com>
2023-05-23 22:32:43 +0800

    1090-hash

--- f12_solution.cpp
+++ f12_solution.cpp
@@ -5,6 +5,7 @@
 #include "stack"
 #include "unordered_set"
 #include "algorithm"
+#include "numeric"
 
 using namespace std;
 
@@ -236,6 +237,28 @@ public:
         if (!nodeRight) node->right = nullptr;
 
         return nodeLeft || nodeRight;
+    }
+
+    int largestValsFromLabels(vector<int>& values, vector<int>& labels, int numWanted, int useLimit) {
+        int n = values.size();
+        vector<int> id(n);
+        std::iota(id.begin(),  id.end(), 0);
+        sort(id.begin(),  id.end(), [&](int i, int j) {
+            return values[i] > values[j];
+        });
+
+        int ans = 0, choose = 0;
+        unordered_map<int, int> cnt;
+        for (int i = 0; i < n && choose < numWanted; ++i) {
+            int label = labels[id[i]];
+            if (cnt[label] == useLimit) continue;
+
+            ++choose;
+            ans += values[id[i]];
+            ++cnt[label];
+        }
+        return ans;
+
     }
 };
 
