df8982bb5d10ab01a967e78ea3ae21d0a06963fa
EvaiGG <2889414582@qq.com>
2023-06-12 01:42:08 +0800

    1054-sliding-window

--- f14_solution.cpp
+++ f14_solution.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <numeric>
 
 using namespace std;
 
@@ -34,6 +35,41 @@ public:
             }
         }
     }
+
+    /**
+     * 1052 老板生气会引起顾客生气，反之满意 求最大满意量
+     * @param customers  顾客数组 i项表示书店开店第i分钟 进来的人数量
+     * @param grumpy 老板的情绪数组  为1时生气
+     * @param minutes 可控制老板连续minutes内 不生气
+     * @return 客户最大满意量
+     */
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int maxS = 0;
+        //
+        for (int i = 0; i < customers.size(); ++i) {
+            if (grumpy[i] == 0) {
+                maxS += customers[i];
+                customers[i] = 0; // 添加过后 赋值为0 方便后续不添加情绪判断 循环过后就只有老板生气的时刻有顾客
+            }
+        }
+
+//        // 让minutes这个时间窗口 在customers中滑动 比较每个时间段里面控制情绪挽单最大量
+//        if(minutes >= customers.size())
+//            return accumulate(customers.begin(),  customers.end(), 0);
+        int temp = 0; // minute 区间
+        int left = 0;
+        int maxV = 0; // 最大区间值
+
+        for (int right = 0; right < customers.size(); ++right) {
+            temp += customers[right];
+            if (right - left + 1 == minutes) {
+                maxV = max(maxV, temp);
+                temp -= customers[left++];
+            }
+        }
+
+        return maxV + maxS;
+    }
 };
 
 
@@ -47,4 +83,9 @@ int main() {
         }
         cout << "\n";
     }
+    vector<int> t({1});
+    vector<int> grumpy({0});
+
+    auto result = solu.maxSatisfied(t, grumpy, 1);
+    cout << result << endl;
 }
